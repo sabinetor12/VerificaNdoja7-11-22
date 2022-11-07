6a8943329e213986fe93e6fd9f08ffce63bbfb9c
lai2-01 <lai2-01@itismeucci.com>
2022-11-07 10:12:45 +0100

    inizio

---
+++ verifica_ndoja_client/src/main/java/itismeucci/fi/App.java
@@ -0,0 +1,13 @@
+package itismeucci.fi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
